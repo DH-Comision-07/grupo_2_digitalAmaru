1b0d9f6490fac1de2d1a2b2c8ef5ef3af4207a5f
HeidiLuisaBernal <bernalh168@gmail.com>
2024-03-12 21:48:15 -0300

    modulo EJS

--- src/app.js
+++ src/app.js
@@ -6,25 +6,27 @@ const port = 3300;
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
+app.set('views engine', 'ejs');
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/index.html"));
+  res.sendFile(path.join(__dirname, "/views/index.ejs"));
 });
 
-app.get("/home", (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/index.html"));
-});
 
-app.listen(port,() => {
-  console.log("Servidor corriendo en el puerto " + port);
-});
 
+
+//ruta de producto
 app.get("/detalle-de-curso", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/product-details.html"));
 });
 
+//ruta de usuario
 app.get("/register", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/register.html"));
 });
+//ruta de usuario
 app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/login.html"));
 });
+
+//vista y ruta de carrito, vista de listado de productos, vista de agregar producto (formulario), edicion de productos, vista perfil de usuario, vista de edicion de usuario (formulario), vista donde enlisten usuarios. cada uno con ruta
