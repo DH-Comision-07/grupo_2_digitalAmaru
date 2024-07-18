d97470a985bdb565baa1e51b444bf008c3007693
Matias Lagunas <matias.lagunas.t@gmail.com>
2024-07-18 15:22:06 -0300

    agregado de controllers

--- src/controllersNew/productController.js
+++ src/controllersNew/productController.js
@@ -14,7 +14,63 @@ const productController = {
             console.error(error);
             res.status(500).send('Error al obtener los productos');
         }
-    }
+    },
+    editionCreate: function(req, res){ 
+            return res.render('editionProducts');
+    },
+    creationProduct: async function (req, res) {
+        try {
+            let product = await db.Product.findByPk(req.params.id); // Cambia 'Product' al nombre de tu modelo
+
+            res.render('product-details', {
+                product: product
+            });
+            
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Error al obtener los productos');
+        }
+    },
+    editProduct : async function (req, res) { 
+    let product = await db.Product.findByPk(req.params.id)
+        .then(function(product){
+            res.render('editproducts/editProduct',{product: product})
+        })
+    
+    .catch(function (req, res) {
+        return res.status(404).send('Producto no encontrado');
+        }
+    )
+        
+   
+},
+create: function (req, res) {
+    db.product.create({
+        name: req.body.name,
+        description: req.body.description,
+        image: req.body.image,
+        price: req.body.price,
+        name_teacher: req.body.name_teacher
+    })
+    res.render('index')
+},
+updateProduct: function (req, res) {
+    db.peliculas.update({
+        name: req.body.name,
+        description: req.body.description,
+        image: req.body.image,
+        price: req.body.price,
+        name_teacher: req.body.name_teacher
+    },
+    {
+    where: {
+        id: req.params.id
+        }
+    })
+    res.redirect('/product/crearueditar')
+}
+
+    
 };
 
 module.exports = productController;
--- src/routes/product.js
+++ src/routes/product.js
@@ -6,13 +6,13 @@ const userValidations = require('../middlewers/validations');
 
 router.get("/detalle/:id", productController.productById)
 
-//router.get('/crearueditar', productController.editionCreate)
+router.get('/crearueditar', productController.editionCreate)
 //editCReate
 
-//router.get('/edit', productController.creationProduct);
-//router.get('/edit/:id', productController.editProduct); // Ruta para obtener el producto a editar
-//router.post('/create', userValidations.productCreate, productController.createProduct); // Ruta para crear un nuevo producto
-//router.post('/edit/:id', userValidations.productCreate, productController.updateProduct); // Ruta para actualizar el producto
+router.get('/edit', productController.creationProduct);
+router.get('/edit/:id', productController.editProduct); // Ruta para obtener el producto a editar
+router.post('/create', userValidations.productCreate, productController.create); // Ruta para crear un nuevo producto
+router.post('/edit/:id', userValidations.productCreate, productController.updateProduct); // Ruta para actualizar el producto
 
 // cart
 //router.get('/cartAdd', productController.viewCart);
